755734df09a3425744d6ce9bd2151786db89713a
Alex <alexvnep@gmail.com>
2024-10-30 14:32:30 +0200

    feat: add names of pokemon to use in game

---
+++ src/components/Data.jsx
@@ -0,0 +1,16 @@
+const pokemonList = [
+  "charizard",
+  "pikachu",
+  "mewtwo",
+  "dragonite",
+  "blastoise",
+  "kakuna",
+  "zapdos",
+  "zubat",
+  "cubone",
+  "voltorb",
+  "vulpix",
+  "ditto",
+];
+
+export default pokemonList;
